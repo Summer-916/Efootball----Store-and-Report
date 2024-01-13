afac66476a389459b0b25949c9bf9cb2be701d34
Summer-916 <ylin073@connect.hkust-gz.edu.cn>
2024-01-13 12:32:10 +0800

    Struct the box -- text_storage

--- text_storage/store.py
+++ text_storage/store.py
@@ -1,19 +1,21 @@
 import pack
 
+file_path = "C:\\Users\\Summer\\Desktop\\Progect\\Efootball----Store-and-Report\\test.txt"
+
 # - initial date
 ini_year = 2024
 ini_month = 1
 ini_date = 13
 
 # - Main function zone
-f = open("test.txt","r")
+f = open(file_path,"r")
 date,month,year=pack.get_date(f)
 f.seek(0)
 new_coin,curr_coin=pack.get_coin(f)
 curr_coin=curr_coin+new_coin
 f.close()
 
-g = open("test.txt","a")
+g = open(file_path,"a")
 pack.write_date(g,date,month,year)
 pack.write_coin(g,new_coin,curr_coin)
 pack.end_line(g)
